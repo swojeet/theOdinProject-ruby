639bd47c30aec813dd2621558eda1b1c62a87535
swojeet <swojeetkayastha@gmail.com>
2017-05-31 23:38:57 +0545

    Implement select method

--- advanced_building_blocks/enumerable_methods.rb
+++ advanced_building_blocks/enumerable_methods.rb
@@ -10,7 +10,16 @@ module Enumerable
       yield self[i], i
     end
   end
+
+  def my_select
+    ans = []
+    self.my_each do |x|
+      ans << x if yield x
+    end
+    ans
+  end
 end
 
 [0,1,2].my_each { |x| puts x}
 [5,1,2,4].my_each_with_index { |x,y| puts "#{x} and #{y}"}
+[5,1,2,4].my_select { |x| x%2 ==0}
