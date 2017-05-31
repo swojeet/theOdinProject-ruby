74309d487c0c1037e8bc0817192e4e499709893c
swojeet <swojeetkayastha@gmail.com>
2017-05-31 23:38:05 +0545

    Implement each_with_index method

--- advanced_building_blocks/enumerable_methods.rb
+++ advanced_building_blocks/enumerable_methods.rb
@@ -4,4 +4,13 @@ module Enumerable
       yield self[i]
     end
   end
+
+  def my_each_with_index
+    for i in 0...self.length
+      yield self[i], i
+    end
+  end
 end
+
+[0,1,2].my_each { |x| puts x}
+[5,1,2,4].my_each_with_index { |x,y| puts "#{x} and #{y}"}
