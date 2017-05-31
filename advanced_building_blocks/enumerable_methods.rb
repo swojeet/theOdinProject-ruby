4f8abf41a84bc7c2d3180087cb661ce792ed56d2
swojeet <swojeetkayastha@gmail.com>
2017-05-31 23:41:08 +0545

    Implement any method

--- advanced_building_blocks/enumerable_methods.rb
+++ advanced_building_blocks/enumerable_methods.rb
@@ -25,9 +25,18 @@ module Enumerable
     end
     return true
   end
+
+  def my_any
+    self.my_each do |x|
+      return true if yield x
+    end
+    return false
+  end
+
 end
 
 [0,1,2].my_each { |x| puts x}
 [5,1,2,4].my_each_with_index { |x,y| puts "#{x} and #{y}"}
 [5,1,2,4].my_select { |x| x%2 ==0}
 [5,1,2,4].my_all { |x| x%2 ==0}
+[2,2,2].my_any { |x| x%3 ==0}
